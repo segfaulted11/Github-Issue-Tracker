1bd4a50d8bcd211a6928deb6ed74d5efac4a6a4e
Seg Fault <segfaulted11@gmail.com>
2026-03-13 16:12:35 +0600

    successfully loaded data the page (deafult n on the all btn)

--- main.js
+++ main.js
@@ -14,22 +14,61 @@ let loadData = ()=>{
 let display = (object)=>{
     let getArray = object.data
 
+    content.innerHTML = "";
+
     getArray.forEach(eachObject => {
 
-    content.innerHTML += `title - ${eachObject.title}`
+       // border based on status
+       let borderClass = "";
 
+       if(eachObject.status ==="open"){
+        borderClass = "border-t-4 border-green-500"; 
+        }
+        if(eachObject.status ==="closed"){
+            borderClass = "border-t-4 border-purple-500"; 
+        }
+
+        // image based on priority
+        let priorityImg = "";
+
+        if(eachObject.priority === "high" || eachObject.priority === "medium"){
+            priorityImg = `<img src="./img/Open-Status.png" class="size-8">`;
+        }else{
+            priorityImg = `<img src="./img/Closed- Status .png" class="size-8">`;
+        }
+
+        // create buttons for each label
+        let labelButtons = eachObject.labels
+            .map(label => `<button class="btn btn-primary mr-2">${label}</button>`)
+            .join("");
+
+        content.innerHTML +=  `
+        <div class="bg-base-100 shadow p-3 h-85 rounded ${borderClass}">
+        <div class="flex justify-between">
+            ${priorityImg}
+            <button class="btn bg-teal-300">${eachObject.priority}</button>
+            </div>
+            <h3 class="font-bold">${eachObject.title}</h3>
+            <h3 class="font-bold opacity-75">${eachObject.description}</h3>
+            ${labelButtons}
+            <hr>
+            <p>${eachObject.author}</p>
+            <p>${new Date(eachObject.createdAt).toLocaleDateString()}</p>
+        </div>
+        `;
     });
 }
 
+//default content
+loadData()
 
 // default selected button
 btn1.classList.add("btn-primary");
 
-//default content
-content.textContent = loadData()
 
 btn1.addEventListener("click", function () {
-    content.textContent = "ALL BUTTON";
+
+    loadData();
 
     btn1.classList.add("btn-primary");
     btn2.classList.remove("btn-primary");
